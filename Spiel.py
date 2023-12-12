5adb2e5528ce40c3cef96e3ae329efbce4172c01
Roman53S <roman.schunk1999@gmail.com>
2023-12-12 11:30:53 +0100

    Edit: __führe_zug_aus

--- Spiel.py
+++ Spiel.py
@@ -21,22 +21,34 @@ class Spiel:
         pass
 
     def start(self):
+        aktueller_spieler = self.spieler1
         self.__erstelle_spieler()
         while True:
             self.schachbrett.anzeigen()
-            self.__führe_zug_aus(self.spieler1)
-            # Spieler1 und Spieler 2 soll abwechselnd einen Zug ausführen
+            self.__führe_zug_aus(aktueller_spieler)
+            
+            # Nach jeder Runde wird der aktuelle Spieler gewechselt.
+            if aktueller_spieler == self.spieler1:
+                aktueller_spieler = self.spieler2
+            else:
+                aktueller_spieler = self.spieler1
 
     def __führe_zug_aus(self, spieler):
         '''
         Führt einen Zug aus, wenn der Zug regelkonform ist.
         '''
-        input(f"{spieler.get_name()} gebe einen Zug ein: ")
-
+        zug_string=input(f"{spieler.get_name()} gebe einen Zug ein (xy xy): ")
+        zug_tupel=self.str2Tuple(zug_string)
+        print(zug_tupel)
     def __add_zug(self, start, ziel, schachfigur):
         self.zug_verlauf.append(self.zug(start, ziel, schachfigur))
         pass
-
+    
+    def str2Tuple(self, string):
+        '''
+        Konvertiert einen String in ein Tupel.
+        '''
+        return tuple(string.split(" "))
 
 if __name__ == "__main__":
     spiel = Spiel()
